caeeb31bfecb1ccc3663a5cddf49593903ee15cc
ch <3165269033@qq.com>
2022-07-16 11:36:06 +0800

    first commit

---
+++ springboot_08_ssmp/src/test/java/com/itheima/dao/BookDaoTestCase.java
@@ -0,0 +1,16 @@
+package com.itheima.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookDaoTestCase {
+  @Autowired
+  private BookDao bookDao;
+    @Test
+    void testGetById(){
+        System.out.println(bookDao.selectById(1));
+
+    }
+}
